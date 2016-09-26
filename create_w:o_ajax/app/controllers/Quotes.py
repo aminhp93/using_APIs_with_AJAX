67a738bf499c71031a3cb83468d8e58532190101
aminhp93 <minhpn.org.ec@gmail.com>
2016-09-26 09:53:58 -0700

    ajax posts

---
+++ create_w:o_ajax/app/controllers/Quotes.py
@@ -0,0 +1,23 @@
+from system.core.controller import *
+class Quotes(Controller):
+    def __init__(self, action):
+        super(Quotes, self).__init__(action)
+        self.load_model('Quote')
+    def index(self):
+        quotes = self.models['Quote'].all();
+        return self.load_view('quotes/index.html', quotes = quotes)
+    # def index_json(self):
+    #     quotes = self.models['Quote'].all()
+    #     return jsonify(quotes=quotes)
+
+    # def index_html(self):
+    # 	quotes = self.models['Quote'].all()
+    # 	return self.load_view('partial/quotes.html', quotes = quotes)
+
+    def create(self):
+        new_quote = {'author': request.form['author'], 'quote': request.form['quote']}
+        self.models['Quote'].create(new_quote)
+
+        quotes = self.models['Quote'].all();
+        return self.load_view('partial/quotes.html', quotes = quotes)
+
---
+++ html/app/controllers/Quotes.py
@@ -0,0 +1,14 @@
+from system.core.controller import *
+class Quotes(Controller):
+    def __init__(self, action):
+        super(Quotes, self).__init__(action)
+        self.load_model('Quote')
+    def index(self):
+        return self.load_view('quotes/index.html')
+    def index_json(self):
+        quotes = self.models['Quote'].all()
+        return jsonify(quotes=quotes)
+
+    def index_html(self):
+    	quotes = self.models['Quote'].all()
+    	return self.load_view('partial/quotes.html', quotes = quotes)
